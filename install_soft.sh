a29c1080c23996fd3d43623551a94a65ac2be620
Jibux <jibux@jibux.info>
2020-10-06 22:38:11 +0200

    Rename to setup_config + add install_soft

---
+++ install_soft.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+#DEBIAN="no"
+BACKPORTS_OPT=""
+
+if hostnamectl | grep -iq debian; then
+	#DEBIAN="yes"
+	BACKPORTS_OPT="-tbuster-backports"
+fi
+
+sudo apt install -y vim vim-common vim-gtk3 vim-gui-common vim-nox vim-runtime vim-tiny
+sudo apt install -y rxvt-unicode-256color
+sudo apt install -y "$BACKPORTS_OPT" tmux
+sudo apt install -y tmux-plugin-manager
+
+command -v npm &>/dev/null && sudo npm -g install instant-markdown-d
+

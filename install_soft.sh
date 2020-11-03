8a67b2be352e777c735f253f283b9641e4848a93
Jibux <jibux@jibux.info>
2020-11-03 20:52:42 +0100

    Add typora to install_soft

--- install_soft.sh
+++ install_soft.sh
@@ -16,3 +16,8 @@ sudo apt install -y tmux-plugin-manager
 
 command -v npm &>/dev/null && sudo npm -g install instant-markdown-d
 
+wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
+sudo add-apt-repository 'deb https://typora.io/linux ./'
+sudo apt-get update
+sudo apt-get install typora
+
